015bec0f9677356af97263ca3f8ef56c6a834516
Juanci <juancarlos@Juans-MacBook-Pro.local>
2015-06-01 19:11:20 -0600

    v2.8 gruntfile - js & sass creator

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////////////////////////
 ///
-///  Cutback Gruntfile v2.7
+///  Cutback Gruntfile v2.8
 ///  A JS Library to easy build Doubleclick Ad Banners
 ///  Moxie Team
 ///
@@ -12,6 +12,11 @@
 
 'use strict';
 
+// _sizesArrayCrud[0] = Initial size
+// _sizesArrayCrud[1] = Banner type
+// _sizesArrayCrud[2] = Expand size (if expand or not)
+// _sizesArrayCrud[3] = Campaign Name
+
 module.exports = function( grunt ) 
 {
 	var _destFolder = 'dest/';
@@ -20,29 +25,51 @@ module.exports = function( grunt )
 	var _sizesArrayFinal = [];
 	var _copyFiles = [];
 	var _campaignName = [];
-	var _separatorCampaignName = "-";
 
-	if (_sizesArrayCrud[0][2] == ""){
-		_separatorCampaignName = "";
-	};
+	var _separatorCampaignNameArray = [];
+	var _separatorExpandSizeArray = [];
+	var _separatorTypeArray = [];
+
+	var _separatorCampaignName = "_";
+	var _separatorExpandSize = "_";
+	var _separatorType = "_";
 
 	for (var i = 0; i < _sizesArrayCrud.length; i++) {
-		_sizesArrayFinal.push(_destFolder+_sizesArrayCrud[i][2]+"/"+_sizesArrayCrud[i][0]+"_"+_sizesArrayCrud[i][1]+_separatorCampaignName+_sizesArrayCrud[i][2]);
-		_campaignName.push(_sizesArrayCrud[i][2]);
+		if (_sizesArrayCrud[i][3] == ""){
+			_separatorCampaignNameArray.push("");
+		} else {
+			_separatorCampaignNameArray.push(_separatorCampaignName);
+		}
+
+		if (_sizesArrayCrud[i][2] == ""){
+			_separatorExpandSizeArray.push("");
+		} else {
+			_separatorExpandSizeArray.push(_separatorExpandSize);
+		}
+
+		if (_sizesArrayCrud[i][1] == ""){
+			_separatorTypeArray.push("");
+		} else {
+			_separatorTypeArray.push(_separatorType);
+		}
+
+		_sizesArrayFinal.push(_destFolder+_sizesArrayCrud[i][3]+"/"+_sizesArrayCrud[i][0]+_separatorTypeArray[i]+_sizesArrayCrud[i][1]+_separatorExpandSizeArray[i]+_sizesArrayCrud[i][2]+_separatorCampaignNameArray[i]+_sizesArrayCrud[i][3]);
+		_campaignName.push(_sizesArrayCrud[i][3]);
 	};
 
 	for( i = 0; i < _sizesArrayFinal.length; ++i ) {
+
 		_copyFiles.push({
 			expand: true, 
 			cwd: _developFolder+'.temp/', 
-			src: [_sizesArrayCrud[i][0]+"_"+_sizesArrayCrud[i][1]+_separatorCampaignName+_sizesArrayCrud[i][2]+'.js'],
+			src: [_sizesArrayCrud[i][0]+_separatorTypeArray[i]+_sizesArrayCrud[i][1]+_separatorExpandSizeArray[i]+_sizesArrayCrud[i][2]+_separatorCampaignNameArray[i]+_sizesArrayCrud[i][3]+'.js'],
 			dest: _sizesArrayFinal[i]+ '/js/',
       		filter: 'isFile'
 		});
 		_copyFiles.push({
 			expand: true, 
 			cwd: _developFolder+'.temp/', 
-			src: [_sizesArrayCrud[i][0]+"_"+_sizesArrayCrud[i][1]+_separatorCampaignName+_sizesArrayCrud[i][2]+'.css'],
+			src: [_sizesArrayCrud[i][0]+_separatorTypeArray[i]+_sizesArrayCrud[i][1]+_separatorExpandSizeArray[i]+_sizesArrayCrud[i][2]+_separatorCampaignNameArray[i]+_sizesArrayCrud[i][3]+'.css'],
 			dest: _sizesArrayFinal[i] + '/css/', 
       		filter: 'isFile'
 		});
@@ -224,13 +251,47 @@ module.exports = function( grunt )
 		var jsFolder = _developFolder+'js/';
 
 		grunt.file.write(_developFolder+'index.html', '<!DOCTYPE html>\n<html lang="en">\n\t<head>\n\t\t<meta charset="UTF-8">\n\t\t<title>@@__SOURCE_PATH__</title>\n\t\t<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">\n\t\t<link rel="stylesheet" href="css/@@__SOURCE_PATH__.css">\n\t</head>\n\t<body>\n\n\t\t<!-- Start your Banner here -->\n\n\t\t<div class="banner" id="banner">\n\t\t\t<div id="collapse-banner">\n\t\t\t\t<h1>Headline Example</h1>\n\t\t\t\t<h2>Small headline</h2>\n\t\t\t</div>\n\t\t</div>\n\n\t\t<!-- Finish your Banner here -->\n\n\t\t<script src="http://s0.2mdn.net/ads/studio/Enabler.js"></script>\n\t\t<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.13.2/TweenMax.min.js"></script>\n\t\t<script src="https://cdn.jsdelivr.net/cutback-js/latest/cutback.min.js"></script>\n\t\t<script src="js/@@__SOURCE_PATH__.js"></script>\n\n\t\t<!-- <script src="https://cdn.jsdelivr.net/cutback-js/latest/cutback-stats.min.js"></script> -->\n\n\t</body>\n</html>');
-		grunt.file.write(sassFolder+'variables.scss', '//Sass variables goes here');
+		grunt.file.write(sassFolder+'variables.scss', '//Sass variables goes here\n\n$opacityCero: 0;\n\n//Background Colors\n$BackgroundWhite: #ffffff;\n\n//Border Colors\n$borderGray: #333333;\n\n\n//Mixins\n\n@mixin border-radius($radius) {\n\t-webkit-border-radius: $radius;\n\t-moz-border-radius: $radius;\n\t-ms-border-radius: $radius;\n\tborder-radius: $radius;\n}');
 		grunt.file.write(jsFolder+'sharedFunctions.js', '//Shared Functions goes here\n\nvar customFunctions = {\n\n}');
 		grunt.file.mkdir(_developFolder+'img/');
 
 		for( i = 0; i < _sizesArrayCrud.length; ++i ) {
-			grunt.file.write(sassFolder+_sizesArrayCrud[i][0]+"_"+_sizesArrayCrud[i][1]+_separatorCampaignName+_sizesArrayCrud[i][2]+'.scss', "//Sass code goes here \n @import 'variables';");
-			grunt.file.write(jsFolder+_sizesArrayCrud[i][0]+"_"+_sizesArrayCrud[i][1]+_separatorCampaignName+_sizesArrayCrud[i][2]+'.js', "//JS code goes here\n\n\n\n//import 'sharedFunctions.js'");
+
+			var collapseSize = _sizesArrayCrud[i][0].split("x");
+			var expandSize = _sizesArrayCrud[i][2].split("x");
+
+			var sassInitialText = '//Sass code goes here \n @import "variables"; \n\n body {\n\tbackground: $BackgroundWhite;\n}\n\n#banner {\n\twidth: 300px;\n\theight: 50px;\n}\n\n#collapse-banner {\n\twidth: '+ (collapseSize[0] - 2) +'px;\n\theight: '+ (collapseSize[1] - 2) +'px;\n\tbackground: $BackgroundWhite;\n\toverflow: hidden;\n\tcursor: pointer;\n\tborder: 1px $borderGray solid;\n\tposition: relative;\n}'+ ( (_sizesArrayCrud[i][1] == "expand" || (_sizesArrayCrud[i][2] != "" && _sizesArrayCrud[i][2] != "full"))  ?  "\n\n#expanded-banner {\n\tposition: absolute;\n\ttop: 0;\n\tleft: 0;\n\twidth: "+ (expandSize[0] - 2) +"px;\n\theight: "+ (expandSize[1] - 2) +";\n\tbackground: $BackgroundWhite;\n\tdisplay: none;\n\topacity: $opacityCero;\n\toverflow: hidden;\n\tborder: 1px $borderGray solid;\n}" : "") + (_sizesArrayCrud[i][2] == "full"  ?  "\n\n#expanded-banner {\n\tposition: absolute;\n\ttop: 0;\n\tleft: 0;\n\twidth: 100%;\n\theight: 100%;\n\tbackground: $BackgroundWhite;\n\tdisplay: none;\n\topacity: $opacityCero;\n\toverflow: hidden;\n\tborder: 1px $borderGray solid;\n}" : "");
+
+			grunt.file.write(sassFolder+_sizesArrayCrud[i][0]+_separatorTypeArray[i]+_sizesArrayCrud[i][1]+_separatorExpandSizeArray[i]+_sizesArrayCrud[i][2]+_separatorCampaignNameArray[i]+_sizesArrayCrud[i][3]+'.scss', sassInitialText);
+
+			var jsInitialText = '//JS code goes here\n\nvar banner = new Banner({\n\t'+ ( _sizesArrayCrud[i][1] != ""  ?  'type: "' + _sizesArrayCrud[i][1] + '",\n\t' : "" ) + ( _sizesArrayCrud[i][1] == "expand" || (_sizesArrayCrud[i][1] == "in-app" && _sizesArrayCrud[i][2] != "") ?  "expand: true,\n\t" : "" ) + ( _sizesArrayCrud[i][2] != "" && _sizesArrayCrud[i][1] == "expand" ?  "finalExpandSize: [0,0,"+expandSize[0]+","+expandSize[1]+"],\n\t" : "" ) + ( _sizesArrayCrud[i][1] == "expand" ?  'hotspotClose: [""],\n\t' : "" ) + ( (_sizesArrayCrud[i][2] != "" || _sizesArrayCrud[i][1] == "expand") ?  'hotspotExpand: [""],\n\t' : "" ) + 'timelines: ["firstTimeline"],\n\telementsToRegister: [\n\t\t{eventType: "click", element: "#identifier", functionToCall: "function"}\n\t],\n\tanimations: {\n\t\tfirstFrame : function(){\n\t\t}'+ ( (_sizesArrayCrud[i][2] != "" || _sizesArrayCrud[i][1] == "expand")  ?  ",\n\t\texpandStartAnimation : function(){\n\t\t},\n\t\tcollapseStartAnimation: function(){\n\t\t}" : "" ) +'\n\t},\n\ttimelinesAnimation: {\n\t\tregister: function(){\n\t\t\ttimelinesArray[0].to("identifier", 0.2, {opacity:1});\n\t\t}\n\t}\n});\n\n//import "sharedFunctions.js"';
+
+			grunt.file.write(jsFolder+_sizesArrayCrud[i][0]+_separatorTypeArray[i]+_sizesArrayCrud[i][1]+_separatorExpandSizeArray[i]+_sizesArrayCrud[i][2]+_separatorCampaignNameArray[i]+_sizesArrayCrud[i][3]+'.js', jsInitialText);
 		}
 	});
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
